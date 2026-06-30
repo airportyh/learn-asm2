831f5540da83656aedbe0303fcf2eb161eca3318
Toby Ho <toby@repl.it>
2026-06-30 03:28:00 +0000

    Replace magic numbers with named constants in torrey_pines.asm

--- torrey_pines.asm
+++ torrey_pines.asm
@@ -1,3 +1,13 @@
+; syscall numbers
+SYS_WRITE equ 1
+SYS_EXIT  equ 60
+
+; file descriptors
+STDOUT    equ 1
+
+; exit status
+EXIT_SUCCESS equ 0
+
 section .data
     msg db "Torrey Pines", 0x0a
     msg_len equ $ - msg
@@ -6,12 +16,12 @@ section .text
     global _start
 
 _start:
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
+    mov rax, SYS_WRITE  ; sys_write
+    mov rdi, STDOUT     ; stdout
     mov rsi, msg
     mov rdx, msg_len
     syscall
 
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi
+    mov rax, SYS_EXIT   ; sys_exit
+    mov rdi, EXIT_SUCCESS
     syscall
